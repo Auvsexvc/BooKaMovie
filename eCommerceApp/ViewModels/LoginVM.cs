a63898504ac187dd5f2919a17fc199f622aa9377
Auvsexvc <auvsexvc@gmail.com>
2022-07-16 11:50:30 +0200

    +login action and view

---
+++ eCommerceApp/ViewModels/LoginVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eCommerceApp.ViewModels
+{
+    public class LoginVM
+    {
+        [Display(Name = "Email address")]
+        [Required(ErrorMessage = "Email address is requiered")]
+        public string EmailAddress { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
